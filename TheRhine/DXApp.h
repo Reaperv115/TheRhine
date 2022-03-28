671b8873ecb83ca89a9969dc3d8c99b1d99697ee
rjs5785 <rjs5785@hotmail.com>
2022-03-28 00:39:21 -0400

    changed around whats going to be the base DirectX class as well as created and started on the GameTimer class

--- TheRhine/DXApp.h
+++ TheRhine/DXApp.h
@@ -3,6 +3,39 @@
 #include "Triangle.h"
 class DXApp
 {
+public:
+	DXApp();
+	DXApp(HINSTANCE hInst);
+	virtual ~DXApp();
+
+	HINSTANCE AppInst() const;
+	HWND MainWnd() const;
+	float AspectRatio() const;
+
+	int Run();
+
+	virtual bool Init();
+	virtual bool OnResize();
+	virtual bool UpdateScene(float dt) = 0;
+	virtual void DrawScene() = 0;
+	virtual LRESULT MsgProch(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam);
+
+protected:
+	bool InitMainWindow();
+	void CalculateFrameStats();
+
+protected:
+	HINSTANCE mAppInst; //application instance handle
+	HWND mMainWnd;		//main window handle
+	bool mAppPaused;	//is the app paused
+	bool mMinimized;	// is the window minimized
+	bool mMaximized;	// is the window maximized
+	bool mResizing;		// is the window being resized
+
+	void InitializeDirectX(HWND hwnd);
+	void InitializeScene();
+	void Render();
+
 	ID3D11Device* device;
 	ID3D11DeviceContext* deviceContext;
 	IDXGISwapChain* swapChain;
@@ -14,11 +47,6 @@ class DXApp
 
 	int width = 900, height = 700;
 
-public:
-	DXApp();
-	~DXApp();
-	void InitializeDirectX(HWND hwnd);
-	void InitializeScene();
-	void Render();
+
 };
 
