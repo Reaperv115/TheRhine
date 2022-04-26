364a6d37aa2b5a913109c7858d68486910471b45
Slaygony <krtslayer@homail.com>
2022-04-26 17:06:32 -0400

    starting on the camera and working on matrices

--- TheRhine/src/Camera.cpp
+++ TheRhine/src/Camera.cpp
@@ -1,2 +1,21 @@
 #include "rpch.h"
 #include "Camera.h"
+
+namespace Rhine
+{
+	XMMATRIX Camera::GetWorld() const
+	{
+		return XMMatrixIdentity();
+	}
+
+	XMMATRIX Camera::GetView() const
+	{
+		return XMMatrixPerspectiveFovLH(FoV, aspectRatio, nearZ, farZ);
+	}
+
+	XMMATRIX Camera::GetProjection()
+	{
+		return XMMATRIX();
+	}
+
+}
--- TheRhine/src/Camera.h
+++ TheRhine/src/Camera.h
@@ -1,9 +1,30 @@
 #pragma once
+#include "rpch.h"
+
+using namespace DirectX;
 
 namespace Rhine
 {
 	class Camera
 	{
+	public:
+		Camera();
+		~Camera();
+
+		XMVECTOR worldUp = { 0.0f, 1.0f, 0.0f };
+		XMVECTOR worldRight = { 1.0f, 0.0f, 0.0f };
+		XMVECTOR worldForward = { 0.0f, 0.0f, 1.0f };
+
+		XMMATRIX world, view, projection;
+
+		XMMATRIX GetWorld() const;
+		XMMATRIX GetView() const;
+		XMMATRIX GetProjection();
+
+		float FoV = XMConvertToDegrees(90.0f);
+		float aspectRatio = 1920 / 1080;
+		float nearZ = 0.1f;
+		float farZ = 10.0f;
 	};
 }
 
